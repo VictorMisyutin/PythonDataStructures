528dbb6d6fe6aeda816c3d68175818cff1a2b3b0
Victor Misyutin <misyutinvictor@gmail.com>
2024-02-13 13:12:10 -0600

    Pasted Started Code for Lab 4

---
+++ Labs/Lab4/Crypt.py
@@ -0,0 +1,28 @@
+# cryptography ( shuffle elements in a list )
+import random
+# for random shuffle
+strMsg = "The Courier is En Route with the Documents"
+# the plaintext secret message
+print (strMsg, "\n")
+# convert the plaintext to lower case
+print (strMsg, "\n")
+print (strMsg, "\n")
+# replace any spaces in the message with an "x"
+plainTxtList = []
+# declare a one - dimensional list
+# populate the list with the characters comprising the message
+for char in strMsg :
+    # append the characters
+    print() 
+print (plainTxtList, "\n")
+# declare the cipher list
+scrambledList = []
+# use list() to cast the plaintext list into the ciphertext list
+# randomly scramble the list of characters
+print (scrambledList, "\n")
+print ("length of the scrambled list, "\n")
+# determine the length of the list
+# print in blocks of seven letters per row
+for index in range(len(scrambledList)) :
+    print ()
+    print ("\n", "\n") ; print ("\n", "\n")
